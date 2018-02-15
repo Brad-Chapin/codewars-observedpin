795f7ee01c8d76a6e1b1d8bf630104034f94bbe5
Brad Chapin <paledoor@yahoo.com>
2018-02-15 12:23:03 -0800

    ...what a mess

--- pin.js
+++ pin.js
@@ -1,31 +1,8 @@
 function getPINs(observed) {
-
-  // let minus1 = [2,3,5,6,8,9];
-  // let plus3 = [1,2,3,4,5,6];
-  // let minus3 = [4,5,6,7,8,9];
-  let test = observed.split("");
-  let options = [observed];
-
-  for (let i = 0; i < test.length; i++){
-    let obs = test.slice(0);
-    if (plus1(i, obs)){
-      options.push(obs);
-      console.log(test);
-    }
-  }
-  return options;
-}
-
-function plus1 (i, obs) {
-  let p1 = [1,2,5,7,8];
-  obs.forEach(function (e) {
-    if (p1.indexOf(parseInt(e)) == -1){
-      return false;
-    } else {
-      obs[i] = (parseInt(e) + 1).toString();
-      return obs.join("");
-    }
-  });
+  //I was unable to solve this problem. Looking at the solutions, I conclude
+  //that the issue is one of math, not one of code; further, I feel that the
+  //problem presented is somewhat far fetched and outside the boundries of
+  //likely real-world challenges.
 }
 
 console.log(getPINs("139")); //expect ["139", "239"]
